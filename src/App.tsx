011bd16f27f9e25ac309d51343e68087a6affdd8
Barry Bakunowicz <bbach7139@gmail.com>
2024-02-08 11:26:28 -0600

    Install + Setup Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
 
@@ -13,8 +12,8 @@ function App() {
   }
 
   return (
-    <div className="container">
-      <h1>Welcome to Tauri!</h1>
+    <div className="container bg-black">
+      <h1 className={`text-lg`}>Welcome to Tauri!</h1>
 
       <div className="row">
         <a href="https://vitejs.dev" target="_blank">
@@ -22,9 +21,6 @@ function App() {
         </a>
         <a href="https://tauri.app" target="_blank">
           <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
 
