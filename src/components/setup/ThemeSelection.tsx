c9dd476ac135aedf97e72a416b90fa7c5ebba870
Barry Bakunowicz <bbach7139@gmail.com>
2024-02-20 10:44:45 -0600

    Initial dark mode toggle (setup screen)

--- src/components/setup/ThemeSelection.tsx
+++ src/components/setup/ThemeSelection.tsx
@@ -1,21 +1,27 @@
 import { Dispatch, SetStateAction } from 'react';
 import { Button } from '../ui/button';
 
+import { MdOutlineWbSunny } from "react-icons/md";
+import { FaMoon } from "react-icons/fa6";
+import { useTheme } from '../providers/ThemeProvider';
+
 interface ThemeSelectionProps {
   setStep: Dispatch<SetStateAction<number>>
 }
 
 export default function ThemeSelection({setStep}: ThemeSelectionProps) {
+  const {setTheme} = useTheme()
 
   function saveTheme() {
     setStep(2)
   }
 
-  function toggleTheme() {
+  function toggleTheme(choice: "light" | "dark") {
+    setTheme(choice)
   }
 
   function selectAccentColor() {
-    
+
   }
 
   return (
@@ -23,11 +29,22 @@ export default function ThemeSelection({setStep}: ThemeSelectionProps) {
       <div className={`my-auto flex flex-col justify-center items-center h-full`}>
         <div className={`flex flex-col items-center mb-4`}>
           <h1 className={`font-semibold text-3xl`}>Select your theme</h1>
-          <p className={`mt-2 text-slate-700`}>{`Choose the theme you'd like to use for the app.`}</p>
+          <p className={`mt-2 text-slate-700 dark:text-slate-300`}>{`Choose the theme you'd like to use for the app.`}</p>
         </div>
-        <div className={`flex flex-row items-center`}>
-          <Button className={`px-4 py-2 rounded-md mr-4`} variant={`outline`}>Light</Button>
-          <Button className={`bg-slate-700 hover:bg-slate-700/90 text-white px-4 py-2 rounded-md`}>Dark</Button>
+        <div className={`flex flex-row items-center w-fit space-x-8`}>
+          <button className={`bg-slate-100 hover:bg-slate-100/90 border-[1px] border-solid border-slate-300 text-black px-4 py-2 rounded-md mr-4 h-24 w-24`} onClick={() => toggleTheme('light')}>
+            <div className={`flex flex-col items-center justify-center`}>
+              <MdOutlineWbSunny/>
+              <span>Light</span>
+            </div>
+          </button>
+          <button className={`bg-slate-700 hover:bg-slate-700/90 text-white px-4 py-2 rounded-md h-24 w-24`} onClick={() => toggleTheme('dark')}>
+            <div className={`flex flex-col items-center justify-center`}>
+              <FaMoon/>
+              <span>Dark</span>
+            </div>
+            
+          </button>
         </div>
         <Button className={`mt-8 text-md bg-sky-500 hover:bg-sky-500/90`} onClick={() => saveTheme()}>Next</Button>
         <button className={`mt-2`} onClick={() => setStep(0)}>
